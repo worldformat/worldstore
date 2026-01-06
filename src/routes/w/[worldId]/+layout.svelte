b8f5a7408844ab3b909593760b01b05b87bb39f4
L. C. Maren <252968197+lcmaren@users.noreply.github.com>
2026-01-06 16:04:58 +0900

    Set title

---
+++ src/routes/w/[worldId]/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from "$app/state";
+
+  let { children } = $props();
+  
+</script>
+
+<svelte:head>
+  <title>{page.params.worldId} – Worldstore</title>
+</svelte:head>
+
+{@render children()}
