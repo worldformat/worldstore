3fb2364cbfe8cd44fb9da1ab1336136200a19673
L. C. Maren <252968197+lcmaren@users.noreply.github.com>
2026-01-06 16:49:56 +0900

    Add WorldMenu

---
+++ src/lib/components/w/WorldMenu.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import HeroiconsEllipsisHorizontal20Solid from '~icons/heroicons/ellipsis-horizontal-20-solid';
+</script>
+
+<div class="isolate inline-flex rounded-lg bg-white shadow-sm dark:divide-white/10 dark:bg-gray-800/50 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/10">
+  <button class="cursor-pointer group relative min-w-0 overflow-hidden rounded-lg p-1.5 text-center text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700 focus:z-10 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white">
+    <HeroiconsEllipsisHorizontal20Solid class="size-5" />
+  </button>
+</div>
--- src/routes/w/[worldId]/+page.svelte
+++ src/routes/w/[worldId]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Prose from '$lib/components/Prose.svelte';
 	import TabBar, { type Tab } from '$lib/components/TabBar.svelte';
+	import WorldMenu from '$lib/components/w/WorldMenu.svelte';
 
 	let { data } = $props();
 
@@ -15,9 +16,10 @@
 	<div class="mb-2">
 		<a href="/" class="text-sm no-underline hover:underline text-secondary">&larr; Back to Worlds</a>
 	</div>
-	<div class="flex justify-between mb-2">
-		<h1 class="mb-0">{data.world.id}</h1>
+	<div class="flex mb-2 gap-x-3">
+		<h1 class="mb-0 flex-1">{data.world.id}</h1>
 		<TabBar {tabs} class="not-prose" />
+		<WorldMenu />
 	</div>
 	<pre>{data.world.content}</pre>
 </Prose>
