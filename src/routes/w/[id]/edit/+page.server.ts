d6edfbc19b108568be9a578dfbf75a4c673fdff3
Yougine <252968197+yougine-esa@users.noreply.github.com>
2026-01-06 23:33:21 +0900

    valdiate world format upon saving

--- src/routes/w/[id]/edit/+page.server.ts
+++ src/routes/w/[id]/edit/+page.server.ts
@@ -3,6 +3,7 @@ import { error, fail, redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { decode } from 'decode-formdata';
 import * as v from 'valibot';
+import { parse } from '$lib/worldformat';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const content = await getWorldContent(params.id);
@@ -24,7 +25,20 @@ export const actions = {
 			return fail(400, { issues: v.flatten(parsed.issues).nested });
 		}
 
-		await updateWorldContent(id, parsed.output.content);
+		const { content } = parsed.output;
+		try {
+			parse(content);
+		} catch (err: any) {
+			return fail(400, { issues: { content: `Invalid world format: ${err.message}` } });
+		}
+
+		try {
+			await updateWorldContent(id, content);
+		} catch (err: any) {
+			console.error(err);
+			return fail(500, { message: `Error saving the world: ${err.message}` });
+		}
+
 		redirect(303, `/w/${id}`);
 	}
 } satisfies Actions;
