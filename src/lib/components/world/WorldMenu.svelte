8920e674d7d73c2e6dcc80ff6239dd67b2aaf823
L. C. Maren <252968197+lcmaren@users.noreply.github.com>
2026-01-06 20:30:57 +0900

    add delete confirmation and toaster

--- src/lib/components/world/WorldMenu.svelte
+++ src/lib/components/world/WorldMenu.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { applyAction, enhance } from '$app/forms';
 	import { onMount } from 'svelte';
 	import HeroiconsEllipsisHorizontal20Solid from '~icons/heroicons/ellipsis-horizontal-20-solid';
+	import { addToast } from '../Toaster.svelte';
 
-  type Props = {
-    worldId: string;
-  }
-  let { worldId }: Props = $props();
+	type Props = {
+		worldId: string;
+	};
+	let { worldId }: Props = $props();
 	let open = $state(false);
 	let root: HTMLElement;
 
@@ -19,6 +20,14 @@
 		document.addEventListener('click', onClick);
 		return () => document.removeEventListener('click', onClick);
 	});
+
+	function onClickDelete(e: MouseEvent) {
+		const ok = confirm(`Delete the world "${worldId}"?\n\nThis will remove the entire state.`);
+
+		if (!ok) {
+			e.preventDefault();
+		}
+	}
 </script>
 
 <div bind:this={root} class="relative">
@@ -35,13 +44,24 @@
 	{#if open}
 		<ul
 			role="menu"
-			class="absolute right-0 origin-top-right rounded-md bg-white py-1 shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+			class="absolute right-0 w-28 origin-top-right rounded-md bg-white py-1 shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
 		>
 			<li>
-        <form method="post" action="/w/{worldId}?/delete" use:enhance>
-          <button role="menuitem" class="btn btn-menu text-rose-500!">Delete</button>
-        </form>
-      </li>
+				<form
+					method="post"
+					action="/w/{worldId}?/delete"
+					use:enhance={() => ({ result }) => {
+						if (result.type === 'redirect') {
+							addToast({ data: { title: 'World deleted' } });
+						}
+						return applyAction(result);
+					}}
+				>
+					<button role="menuitem" class="btn btn-menu text-rose-500!" onclick={onClickDelete}
+						>Delete world</button
+					>
+				</form>
+			</li>
 		</ul>
 	{/if}
 </div>
