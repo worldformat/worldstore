8129f5049d6f7f35c3a4a70c174963ee5078babf
L. C. Maren <252968197+lcmaren@users.noreply.github.com>
2026-01-08 01:16:11 +0900

    GCS getWorlds

--- src/lib/server/worldstore/gcs.ts
+++ src/lib/server/worldstore/gcs.ts
@@ -1,22 +1,48 @@
-import type { Worldstore } from ".";
+import type { Worldstore } from '.';
+import { Storage, type GetFilesOptions } from '@google-cloud/storage';
 
 export class GCSWorldstore implements Worldstore {
-  createWorld(id: string, content: string): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  getWorlds(): Promise<{ id: string; }[]> {
-    throw new Error("Method not implemented.");
-  }
-  hasWorld(id: string): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
-  getWorldContent(id: string): Promise<string | null> {
-    throw new Error("Method not implemented.");
-  }
-  updateWorldContent(id: string, content: string): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  deleteWorld(id: string): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
+	private storage: Storage;
+
+	constructor(
+		private bucket: string,
+		private prefix?: string
+	) {
+		this.storage = new Storage();
+	}
+
+	createWorld(id: string, content: string): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+
+	async getWorlds(): Promise<{ id: string }[]> {
+		const options: GetFilesOptions = { prefix: this.prefix };
+		const [files] = await this.storage.bucket(this.bucket).getFiles(options);
+		return files
+			.filter((file) => file.name.endsWith('.world'))
+			.map((file) => {
+				let name = file.name;
+
+				if (this.prefix && name.startsWith(this.prefix)) {
+					name = name.slice(this.prefix.length);
+				}
+
+				const id = name.replace(/\.world$/, '');
+
+				return { id };
+			});
+	}
+
+	hasWorld(id: string): Promise<boolean> {
+		throw new Error('Method not implemented.');
+	}
+	getWorldContent(id: string): Promise<string | null> {
+		throw new Error('Method not implemented.');
+	}
+	updateWorldContent(id: string, content: string): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+	deleteWorld(id: string): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
 }
