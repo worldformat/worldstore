4b5ca1b0779d42ad767e4ff5da4b503e2810b017
L. C. Maren <252968197+lcmaren@users.noreply.github.com>
2026-01-13 16:29:02 +0900

    show sign out menu

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,25 @@
-import { requireLogin } from "$lib/server/utils";
-import { worldstore } from "$lib/server/worldstore";
-import type { PageServerLoad } from "./$types";
-
+import { requireLogin } from '$lib/server/utils';
+import { worldstore } from '$lib/server/worldstore';
+import { redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { invalidateSession } from '$lib/server/auth';
+import { deleteSessionCookie } from '$lib/server/cookie';
 
 export const load: PageServerLoad = async () => {
-  requireLogin();
-  const worlds = await worldstore.getWorlds();
-  return { worlds };
+	requireLogin();
+	const worlds = await worldstore.getWorlds();
+	return { worlds };
 };
+
+export const actions = {
+	signOut: async (event) => {
+		if (!event.locals.session) {
+			redirect(302, '/login');
+		}
+
+		await invalidateSession();
+		deleteSessionCookie(event);
+
+		redirect(302, '/login');
+	}
+} satisfies Actions;
